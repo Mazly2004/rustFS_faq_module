964b7956454b9532e414293085f1db4952165631
Oliver Mazorodze <olivermazorodze1@gmail.com>
2026-03-25 11:08:57 +0200

    initial

--- src/main/java/org/innov/rustfs_faq/RustFsFaqApplication.java
+++ src/main/java/org/innov/rustfs_faq/RustFsFaqApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RustFsFaqApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RustFsFaqApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RustFsFaqApplication.class, args);
+    }
 
 }
--- src/test/java/org/innov/rustfs_faq/RustFsFaqApplicationTests.java
+++ src/test/java/org/innov/rustfs_faq/RustFsFaqApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RustFsFaqApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

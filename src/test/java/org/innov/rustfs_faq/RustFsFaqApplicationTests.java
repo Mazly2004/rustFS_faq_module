d408d8409117ebd235faa846b6de2cb4442b18ae
Oliver Mazorodze <olivermazorodze1@gmail.com>
2026-03-25 11:08:51 +0200

    INitial
    initial

---
+++ src/test/java/org/innov/rustfs_faq/RustFsFaqApplicationTests.java
@@ -0,0 +1,13 @@
+package org.innov.rustfs_faq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RustFsFaqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

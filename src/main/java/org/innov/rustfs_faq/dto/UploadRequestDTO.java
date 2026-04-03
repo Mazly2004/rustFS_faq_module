ec27930452242cdbfc44211572e7a87e423f09c9
Nyasha Gandah <ngandah04@gmail.com>
2026-04-03 13:49:03 +0200

    add validation to upload request and enhance HomeService

--- src/main/java/org/innov/rustfs_faq/dto/UploadRequestDTO.java
+++ src/main/java/org/innov/rustfs_faq/dto/UploadRequestDTO.java
@@ -2,17 +2,25 @@ package org.innov.rustfs_faq.dto;
 
 import jakarta.annotation.Nullable;
 import org.springframework.web.multipart.MultipartFile;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UploadRequestDTO {
 
+    @NotNull(message = "File is required")
     private MultipartFile file;
+
+    @NotBlank(message = "Category is required")
     private String category;
+
+    @NotBlank(message = "Language is required")
     private String language;
+
+    @NotBlank(message = "Version is required")
     private String version;
 
     @Nullable
     private String documentId;
-
 }

bf6fb55cef2f66aceb06bda3be3e82cbd820270d
Nyasha Gandah <ngandah04@gmail>
2026-04-03 13:20:53 +0200

    test

---
+++ src/main/java/org/innov/rustfs_faq/RustFsFaqApplication.java
@@ -0,0 +1,13 @@
+package org.innov.rustfs_faq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RustFsFaqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RustFsFaqApplication.class, args);
+    }
+
+}
